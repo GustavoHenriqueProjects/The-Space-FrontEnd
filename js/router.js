44d2fc663de67cfd9e0a78a58d71634bc6478ca8
Gustavo <gustavo326projetos@gmail.com>
2023-04-30 10:47:51 -0300

    Atualizando rotas

--- js/router.js
+++ js/router.js
@@ -1,22 +1,72 @@
 'use strict'
 
+/**
+ * const routes = { '/': '../pages/home.html' }: 
+ * define um objeto "routes" que mapeia os caminhos das rotas
+ *  para os arquivos HTML correspondentes. Neste caso, a raiz 
+ * do site "/" está mapeada para "../pages/home.html".
+ * 
+ */
 const routes = {
-    '/' : '../pages/home.html'
+    '/' : '../pages/home.html',
+    '/astronaut' : '../pages/astronaut.html'
 }
 
+let currentPath = '/'
+
+/**
+ * const route = async () => { ... }: define uma função "route()" 
+ * que é chamada quando um link é clicado. É uma função assíncrona,
+ *  o que permite que ela faça requisições assíncronas ao servidor 
+ * sem bloquear a execução do código.
+ */
 const route = async () =>{
-    //Ouve o evento e corta a ação do link
+    /**
+     * window.event.preventDefault(): impede que o link seja aberto
+     *  normalmente no navegador, já que queremos que a navegação seja
+     *  tratada pelo JavaScript.
+     */
     window.event.preventDefault()
+
+    /**
+     * window.history.pushState({},"", window.event.target.href): 
+     * atualiza o histórico do navegador com a nova URL, para que
+     *  o usuário possa navegar pelo histórico do navegador
+     */
     window.history.pushState({},"", window.event.target.href)
 
+    /**
+     * const path = window.location.pathname:
+     *  pega o caminho do URL atual.
+     * */
     const path = window.location.pathname
-    const route = routes[path] // Pega o endereço
+
+
+    /**
+     * const route = routes[path]: verifica se existe um arquivo HTML
+     *  correspondente ao caminho atual definido no objeto "routes". 
+     * Neste caso, se o caminho for "/", irá retornar "../pages/home.html"
+     */
+    const route = routes[path]
+    console.log("o route"+route)
+
+    /**
+     * const response = await fetch(route): faz uma requisição HTTP para obter
+     *  o conteúdo do arquivo HTML.
+     */
     const response = await fetch(route)
+
+    /**
+     * const html = await response.text(): 
+     * obtém o conteúdo do arquivo HTML como uma string de texto.
+     */
     const html = await response.text() // Tranforma em elemento html
 
-    console.log(html)
+    /**document.getElementById('root').innerHTML = html:
+     *  define o conteúdo da tag HTML com o ID "root" como o
+     *  conteúdo HTML obtido da requisição */
     document.getElementById('root').innerHTML = html
 }
 
-//Torna a função global para ser usada no html
+//Torna a função global para ser usada no html 
 window.route = route
